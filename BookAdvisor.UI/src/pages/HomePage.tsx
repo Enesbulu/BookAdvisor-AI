5a24b6bedb697ece562ee60dd049d910dc34096a
Enes BULUT <bulut0258@gmail.com>
2026-02-05 19:28:05 +0300

    feat(ui): Add 'Create Reading List' Modal functionality
    - Feat: 'Yeni Liste Oluştur' butonu ve Modal tasarımı eklendi.
    - Feat: Mantine useDisclosure ve useForm ile form yönetimi sağlandı.
    - Feat: ReadingListService.create metodu eklendi.
    - UX: Başarılı işlem sonrası Notification (Bildirim) ve Liste Yenileme eklendi.

--- BookAdvisor.UI/src/pages/HomePage.tsx
+++ BookAdvisor.UI/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { ReadingListService, type ReadingListDto } from "../services/readingList.service";
 import { authService } from "../services/auth.service";
-import { Alert, Button, Card, Container, Group, Loader, SimpleGrid, Text, Title } from "@mantine/core";
+import { Alert, Button, Card, Container, Group, Loader, Modal, Notification, SimpleGrid, Text, TextInput, Title } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
-import { IconBook, IconLogout } from "@tabler/icons-react";
+import { IconBook, IconCheck, IconLogout, IconPlus } from "@tabler/icons-react";
+import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
 
 
 export function HomePage() {
@@ -11,11 +13,27 @@ export function HomePage() {
     const [lists, setLists] = useState<ReadingListDto[]>([]);   //backend dengelen listeleri tutar
     const [loading, setLoading] = useState(true);   //veri çekme sırasında loading simgesi göstermek için
 
+    //Modal Kontrolü (useDisclosure)
+    // opened: Modal açık mı? (true/false)
+    // open: Modalı açan fonksiyon
+    // close: Modalı kapatan fonksiyon
+    const [opened, { open, close }] = useDisclosure(false);
+    const [showSuccess, setShowSuccess] = useState(false);  // Bildirim göstermek için state
+
+    //Yeni liste oluşturma formu
+    const form = useForm({
+        initialValues: { name: '' },
+        validate: {
+            name: (value) => (value.length < 3 ? 'Liste adı en az 3 karakter olmalıdır.' : null)
+        },
+    });
+
     //sayfa ilk açıldığında bir kez çalışır.
     useEffect(() => {
         fetchLists();
     }, []);
 
+    ///Bütün okumaListesi ni çekme-listeleme işlemi için metod 
     const fetchLists = async () => {
         try {
             const data = await ReadingListService.getAll();
@@ -34,42 +52,91 @@ export function HomePage() {
         navigate('/login');
     };
 
+    //Yeni liste ekleme fonksiyonu
+    const handleCreateList = async (value: typeof form.values) => {
+        try {
+            //backend'e gönderme
+            await ReadingListService.create({ name: value.name });
+
+            //modal kapatma
+            close();
+            form.reset();
+
+            //Başarı mesajı gösterme, süreli olarak
+            setShowSuccess(true);
+            setTimeout(() => setShowSuccess(false), 3000);  //3 sn sonra kapat
+
+            //Listeyi yenile
+            fetchLists();
+
+        } catch (err) {
+            console.error("Liste oluşturulurken bir hata gerçekleşti: ", err);
+            alert("Hata oluştu!");
+        }
+    }
+
     return (
         <Container size="lg" py="xl">
+
+
+            {/* Modal Bileşeni */}
+            <Modal opened={opened} onClose={close} title="Yeni Okuma Listesi Oluşturma" centered>
+                <form onSubmit={form.onSubmit(handleCreateList)}>
+                    <TextInput label="Liste Adı" placeholder="Örn: Yaz Tatlili Kitapları" data-autofocus required {...form.getInputProps('name')} />
+
+                    <Group justify="flex-end" mt="md">
+                        <Button variant="default" onClick={close}>İptal</Button>
+                        <Button type="submit" >Oluştur</Button>
+                    </Group>
+                </form>
+            </Modal>
+
+            {/* Form oluşturma başarı bildirimi */}
+            {showSuccess && (
+                <Notification icon={<IconCheck size={18} />} color="teal" title="Başarılı" onClose={() => setShowSuccess(false)} mb="md">
+                    Yeni Liste Başarıyla Oluşturuldu.
+                </Notification >
+            )}
+
             {/* Üst Başlık ve Çıkış Butonu */}
             <Group justify="space-between" mb="xl">
                 <Title order={2}>Kütüphanem</Title>
-                <Button color="red" variant="light" leftSection={<IconLogout size={18} />} onClick={handleLogout}>Çıkış Yap</Button>
+                <Group>
+                    <Button leftSection={<IconPlus size={18} />} onClick={open}>Yeni Liste</Button>
+                    <Button color="red" variant="light" leftSection={<IconLogout size={18} />} onClick={handleLogout}>Çıkış Yap</Button>
+                </Group>
             </Group>
             {/* Yükleniyor durumu */}
-            {loading ? (
-                <Group justify="center">
-                    <Loader size="lg" />
-                </Group>
-            ) : (
-                <>
-                    {/* Liste Boşsa Uyarı Göster */}
-                    {lists.length === 0 ? (
-                        <Alert title="Listeniz Boş" color="blue">Henüz hiç okuma listesi oluşturmadınız.</Alert>
+            {
+                loading ? (
+                    <Group justify="center">
+                        <Loader size="lg" />
+                    </Group>
+                ) : (
+                    <>
+                        {/* Liste Boşsa Uyarı Göster */}
+                        {lists.length === 0 ? (
+                            <Alert title="Listeniz Boş" color="blue">Henüz hiç okuma listesi oluşturmadınız.</Alert>
 
-                    ) : (
-                        //MAP fonksiyonu
-                        <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
-                            {lists.map((list) => (
-                                <Card key={list.id} shadow="sm" padding="lg" radius="md" withBorder>
-                                    <Group justify="space-between" mt="md" mb="xs">
-                                        <Text fw={500}>{list.name}</Text>
-                                        <IconBook size={20} color="gray" />
-                                    </Group>
-                                    <Text size="sm" c="dimmed">Bu liste {list.bookCount} adet kitap içerir.</Text>
+                        ) : (
+                            //MAP fonksiyonu
+                            <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
+                                {lists.map((list) => (
+                                    <Card key={list.id} shadow="sm" padding="lg" radius="md" withBorder>
+                                        <Group justify="space-between" mt="md" mb="xs">
+                                            <Text fw={500}>{list.name}</Text>
+                                            <IconBook size={20} color="gray" />
+                                        </Group>
+                                        <Text size="sm" c="dimmed">Bu liste {list.bookCount} adet kitap içerir.</Text>
 
-                                    <Button color="blue" fullWidth mt="md" radius="md">Detayları gör</Button>
-                                </Card>
-                            ))}
-                        </SimpleGrid>
-                    )}
-                </>
-            )}
-        </Container>
+                                        <Button color="blue" fullWidth mt="md" radius="md">Detayları gör</Button>
+                                    </Card>
+                                ))}
+                            </SimpleGrid>
+                        )}
+                    </>
+                )
+            }
+        </Container >
     );
 }
--- BookAdvisor.UI/src/services/readingList.service.ts
+++ BookAdvisor.UI/src/services/readingList.service.ts
@@ -1,19 +1,32 @@
 import api from './api';
 
-//backend den gelecek veri için dto
+//backend den GET-ReadingLists gelecek veri için dto
 export interface ReadingListDto {
     id: string;
     name: string;
     bookCount: number;
 }
 
-//backend istek servisi
+//Create ReadingLsit için kullanılan Dto yapısı
+export interface CreateReadingListRequest {
+    name: string;
+}
+
+///Backend istek servisi
 export const ReadingListService = {
+    ///Bütün ReadingList leri listeleme metodu
     //GET /api/reading-lists
     getAll: async () => {
         const response = await api.get<ReadingListDto[]>('/reading-lists');
         return response;
     },
+
+    ///ReadingList Oluşturma metodu
+    //POST /api/reading-lists
+    create: async (data: CreateReadingListRequest) => {
+        const response = await api.post<string>('/reading-lists', data);
+        return response.data;
+    }
 };
 
 
